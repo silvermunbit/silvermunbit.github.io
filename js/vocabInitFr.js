811213b1f45aae4b1dc994622a90aea34dee0daf
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2022-02-05 20:57:27 +0100

    Create vocabInitFr.js

---
+++ js/vocabInitFr.js
@@ -0,0 +1,10 @@
+var t1 = new Array()
+$.get("../../txt/arrayFr1.txt", function (data) {
+    t1 = data.split('; ')
+    $("#total").html("Tous les mots: " + t1.length)
+})
+var t2 = new Array()
+$.get("../../txt/arrayEn2.txt", function (data) { t2 = data.split('; ') })
+var correct = 0
+var correctText = "Mots corrects: "
+$("#guessed").html(correctText + correct)
