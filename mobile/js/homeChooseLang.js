d75acc1a4ad04fceab7af6e30273359ea0814aca
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2022-02-05 20:35:45 +0100

    Update homeChooseLang.js

--- mobile/js/homeChooseLang.js
+++ mobile/js/homeChooseLang.js
@@ -1,17 +1,27 @@
-﻿$("#chooseEn").click(function () {
+$("#chooseEn").click(function () {
     if ($("#submenuEn").hasClass("slide")) $("#submenuEn").removeClass("slide")
     else $("#submenuEn").addClass("slide")
+    $("#submenuFr").removeClass("slide")
+    $("#submenuJa").removeClass("slide")
+    $("#submenuKo").removeClass("slide")
+})
+$("#chooseFr").click(function () {
+    $("#submenuEn").removeClass("slide")
+    if ($("#submenuFr").hasClass("slide")) $("#submenuFr").removeClass("slide")
+    else $("#submenuFr").addClass("slide")
     $("#submenuJa").removeClass("slide")
     $("#submenuKo").removeClass("slide")
 })
 $("#chooseJa").click(function () {
     $("#submenuEn").removeClass("slide")
+    $("#submenuFr").removeClass("slide")
     if ($("#submenuJa").hasClass("slide")) $("#submenuJa").removeClass("slide")
     else $("#submenuJa").addClass("slide")
     $("#submenuKo").removeClass("slide")
 })
 $("#chooseKo").click(function () {
     $("#submenuEn").removeClass("slide")
+    $("#submenuFr").removeClass("slide")
     $("#submenuJa").removeClass("slide")
     if ($("#submenuKo").hasClass("slide")) $("#submenuKo").removeClass("slide")
     else $("#submenuKo").addClass("slide")
