e893d826f6223f1ce74e089aa1ae609b25f86af0
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2022-07-11 14:10:57 +0200

    Update homeTheme.js

--- js/homeTheme.js
+++ js/homeTheme.js
@@ -4,7 +4,7 @@
     $("h2").css("color", "black")
     $("h3").css("color", "#a1a1a1")
     $("footer").css("color", "#a1a1a1")
-    $("#menu").css("background", "#ccc")
+    $("#menu").css("background", "#ddd")
     $("#menu").find("a").css("color", "black")
     $("#menu").find("a").addClass("light")
     $("#settingsButton").attr("src", "gfx/settingsLight.png")
