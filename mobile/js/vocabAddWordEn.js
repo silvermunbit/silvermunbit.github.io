c90f398d46bdb89cb74c5dbca10114b6cbf6d7d8
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2024-08-24 18:57:21 +0200

    Delete mobile/js/vocabAddWordEn.js

--- mobile/js/vocabAddWordEn.js
+++
@@ -1,8 +0,0 @@
-﻿$("#wordAddBt").click(function () {
-    var word1 = $("#wordAdd1").val()
-    var word2 = $("#wordAdd2").val()
-    console.log(word1, word2, t1, t2)
-    t1.push(word1)
-    t2.push(word2)
-    console.log(t1, t2)
-})
