84ac899a5b8d03790f42b5356dbc03cf1ea3f45f
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2025-04-02 16:29:23 +0200

    Update homeTheme.js

--- js/homeTheme.js
+++ js/homeTheme.js
@@ -1,22 +1,18 @@
 ﻿var lightTheme = function () {
     $("body").css("background", "#eeeeee")
     $("h1").css("color", "black")
-    $("h2").css("color", "black")
+    $(".langMenu").find("li").css("color", "black")
     $("#menu").css("background", "#ddd")
     $("#menu").find("a").css("color", "black")
     $("#menu").find("a").addClass("light")
-    $(".chooseLang").find("a").addClass("light")
-    $(".submenuText").css("color", "black")
     $("path").addClass("light")
 }
 var darkTheme = function () {
     $("body").css("background", "#111")
     $("h1").css("color", "white")
-    $("h2").css("color", "white")
+    $(".langMenu").find("li").css("color", "white")
     $("#menu").css("background", "#222")
     $("#menu").find("a").css("color", "white")
     $("#menu").find("a").removeClass("light")
-    $(".chooseLang").find("a").removeClass("light")
-    $(".submenuText").css("color", "white")
     $("path").removeClass("light")
 }
