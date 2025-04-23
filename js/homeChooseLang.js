a0c9c325dcc114b7d8f3da433f6595235daf19e7
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2025-04-23 15:55:36 +0200

    Update homeChooseLang.js

--- js/homeChooseLang.js
+++ js/homeChooseLang.js
@@ -1,39 +1,5 @@
-﻿$("#chooseEn").click(function () {
-    if ($("#submenuPo").hasClass("slide")) $("#submenuPo").removeClass("slide")
-    else $("#submenuPo").addClass("slide")
-    $("#submenuFr").removeClass("slide")
-    $("#submenuJa").removeClass("slide")
-    $("#submenuKo").removeClass("slide")
-})
-$("#chooseFr").click(function () {
-    $("#submenuPo").removeClass("slide")
-    if ($("#submenuFr").hasClass("slide")) $("#submenuFr").removeClass("slide")
-    else $("#submenuFr").addClass("slide")
-    $("#submenuJa").removeClass("slide")
-    $("#submenuKo").removeClass("slide")
-})
-$("#chooseJa").click(function () {
-    $("#submenuPo").removeClass("slide")
-    $("#submenuFr").removeClass("slide")
-    if ($("#submenuJa").hasClass("slide")) $("#submenuJa").removeClass("slide")
-    else $("#submenuJa").addClass("slide")
-    $("#submenuKo").removeClass("slide")
-})
-$("#chooseKo").click(function () {
-    $("#submenuPo").removeClass("slide")
-    $("#submenuFr").removeClass("slide")
-    $("#submenuJa").removeClass("slide")
-    if ($("#submenuKo").hasClass("slide")) $("#submenuKo").removeClass("slide")
-    else $("#submenuKo").addClass("slide")
-})
-
-$("#chooseWo").click(function () {
-    $("#submenuLa").removeClass("slide")
-    if ($("#submenuWo").hasClass("slide")) $("#submenuWo").removeClass("slide")
-    else $("#submenuWo").addClass("slide")
-})
-$("#chooseLa").click(function () {
-    $("#submenuWo").removeClass("slide")
-    if ($("#submenuLa").hasClass("slide")) $("#submenuLa").removeClass("slide")
-    else $("#submenuLa").addClass("slide")
+﻿$("#lalLink1").hover(function () {
+    $("#lalLine1").css("color", "red");
+}, function () {
+    $("#lalLine1").css("color", "blue");
 })
