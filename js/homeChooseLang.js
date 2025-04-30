ace2ab9993ff867ebba665f032d4a99a8412ad11
silvermunbit <35703791+silvermunbit@users.noreply.github.com>
2025-04-30 12:52:07 +0200

    Update homeChooseLang.js

--- js/homeChooseLang.js
+++ js/homeChooseLang.js
@@ -3,3 +3,8 @@
 }, function () {
     $("#lalLine1").css("visibility", "hidden");
 })
+$("#lalLink2").hover(function () {
+    $("#lalLine2").css("visibility", "visible");
+}, function () {
+    $("#lalLine2").css("visibility", "hidden");
+})
